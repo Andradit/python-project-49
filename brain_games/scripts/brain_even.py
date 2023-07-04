a78539006abc38ac4e1412bc019b1abe177f7488
Andradit <drakushev@mail.ru>
2023-07-04 13:11:36 +0300

    add step 2

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,31 @@
+import random
+
+
+def main():
+    print("Welcome to the Brain Games!")
+    print("May I have your name?")
+    name = input()
+    print(f'Hello, {name}!')
+    print('Answer "yes" if the number is even, otherwise answer "no"')
+    for n in range(0, 3):
+        number = random.randint(0, 100)
+        print(f"Question: {number}")
+        print("Your answer: ")
+        answer = input()
+        correct_answer = ""
+        if number % 2 == 0:
+            correct_answer = "yes"
+        else:
+            correct_answer = "no"
+        if answer == correct_answer:
+            print("Correct!")
+        else:
+            print(f"{answer} is wrong answer ;(. Correct answer was"
+                f"{correct_answer}."
+                f"Let's try again, {name}!")
+            return
+    print(f"Congratulations, {name}!")
+
+
+if __name__ == '__main__':
+    main()
