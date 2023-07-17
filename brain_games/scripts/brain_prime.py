dc601b285a9fcd12b41a4cca10654e12afc30b9f
Andradit <drakushev@mail.ru>
2023-07-18 01:19:01 +0300

    add step 9

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import random
+import prompt
+
+
+def main():
+    print("Welcome to the Brain Games!")
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    print('Answer "yes" if given number is prime. Otherwise answer "no"')
+    for n in range(0, 3):
+        num = random.randint(0, 50)
+        print(f"Question: {num}")
+        print("Your answer: ")
+        answer = input()
+        correct_answer = ""
+        for i in range(2, num):
+            if num % i != 0:
+                correct_answer = "yes"
+            else:
+                correct_answer = "no"
+                break
+        if num == 2:
+            correct_answer = "yes"
+        if answer == correct_answer:
+            print("Correct!")
+        else:
+            print(f"'{answer}' is wrong answer ;(. "
+                  f"Correct answer was '{correct_answer}'. "
+                  f"Let's try again, {name}!")
+            return
+    print(f"Congratulations, {name}!")
+
+
+if __name__ == '__main__':
+    main()
