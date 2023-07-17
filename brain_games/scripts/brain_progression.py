f911dc377025e6b55aec95e973b1f199f9ae7fa3
Andradit <drakushev@mail.ru>
2023-07-18 01:15:28 +0300

    add quotation marks

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -26,8 +26,8 @@ def main():
         if answer == correct_answer:
             print("Correct!")
         else:
-            print(f"{answer} is wrong answer ;(. "
-                  f"Correct answer was {correct_answer}. "
+            print(f"'{answer}' is wrong answer ;(. "
+                  f"Correct answer was '{correct_answer}'. "
                   f"Let's try again, {name}!")
             return
     print(f"Congratulations, {name}!")
