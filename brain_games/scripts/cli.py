49d6807a998f8c94fb764413f9ba1b1ead6c89ed
Andradit <drakushev@mail.ru>
2023-07-19 00:34:45 +0300

    fix cli.py

--- brain_games/scripts/cli.py
+++ brain_games/scripts/cli.py
@@ -2,7 +2,8 @@ import prompt
 
 
 def welcome_user():
-    prompt.string('May I have your name? ')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
 
 
 welcome_user()
