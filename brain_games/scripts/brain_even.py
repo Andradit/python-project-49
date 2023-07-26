1f70e7b3018d8673d3b6156f81bc467d09ac2be0
Andradit <drakushev@mail.ru>
2023-07-26 22:27:24 +0300

    fix brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -5,8 +5,8 @@ from brain_games.games import even
 
 
 def main():
-    engine.run_game(even.generate_question, "Answer 'yes' if the "
-                    "number is even, otherwise answer 'no'.")
+    engine.run_game(even.generate_question, 'Answer "yes" if the '
+                    'number is even, otherwise answer "no".')
 
 
 if __name__ == '__main__':
