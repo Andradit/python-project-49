4873249ac00b2cf0f438caa580689fda38947ac9
Andradit <drakushev@mail.ru>
2023-07-26 22:30:06 +0300

    fix brain_prime.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -5,8 +5,8 @@ from brain_games.games import prime
 
 
 def main():
-    engine.run_game(prime.generate_question, "Answer 'yes' if given "
-                    "number is prime. Otherwise answer 'no'.")
+    engine.run_game(prime.generate_question, 'Answer "yes" if given '
+                    'number is prime. Otherwise answer "no".')
 
 
 if __name__ == '__main__':
